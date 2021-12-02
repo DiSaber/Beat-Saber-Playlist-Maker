7dd2a4b11df0fe4abd517413ba822fce44cca664
Di Saber <marc.a.benamou@gmail.com>
2021-12-02 11:46:56 -0600

    Made the getMap function determine whether it's an id or hash

--- src/App.svelte
+++ src/App.svelte
@@ -14,27 +14,25 @@
   		return response.json();
 	}
 	
-	async function getMap(id = '', hash='') {
+	async function getMap(input = '') {
 		// https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
 
-		if (id) {
-  			const response = await fetch('https://api.beatsaver.com/maps/id/' + id, {cache: 'default'});
-			return response.json();
-		} else if (hash) {
-			const response = await fetch('https://api.beatsaver.com/maps/hash/' + hash, {cache: 'default'});
+		if (input.length < 40) {
+  			const response = await fetch('https://api.beatsaver.com/maps/id/' + input, {cache: 'default'});
 			return response.json();
 		} else {
-			return 'null'
+			const response = await fetch('https://api.beatsaver.com/maps/hash/' + input, {cache: 'default'});
+			return response.json();
 		}
 	}
 	
-	getMap('', 'ed876b42a223d1fc0a141c82c916d785a9a7f55f').then(response => data = response)
+	getMap('ed876b42a223d1fc0a141c82c916d785a9a7f55f').then(response => data = response)
 </script>
 
 <h1>Id: {data?.id}</h1>
 <h1>Name: {data?.name}</h1>
 <h1>Hash: {data?.versions[0]?.hash}</h1>
 <textarea bind:value={currentId}></textarea>
-<button on:click={getMap(currentId, '').then(response => data = response)}>
+<button on:click={getMap(currentId).then(response => data = response)}>
 	Refresh
 </button>
